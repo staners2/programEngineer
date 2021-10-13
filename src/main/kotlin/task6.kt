1e950c6f18d4c143a57e6f847ceb19103b0b6bab
Dmitry Aratin <dima.aratin0@gmail.com>
2021-10-13 16:38:26 +0300

    #1 №6

--- src/main/kotlin/task6.kt
+++ src/main/kotlin/task6.kt
@@ -1,6 +1,13 @@
 fun main(args: Array<String>){
-    val list : List<String>? = if (args.isNullOrEmpty()) readLine()?.split(" ")?.toList() else args.toList()
-    list.groupingBy { it }.eachCount().toSortedMap().toList().sortedByDescending { (key, value) -> value }.toMap().forEach(){
+    val list : List<String>? = if (args.isEmpty()) readLine()?.split(" ")?.toList() else args.toList()
+
+    if (list?.get(0) != "") {
+        show(list)
+    }
+}
+
+fun show(list: List<String>?){
+    list?.groupingBy { it }?.eachCount()?.toSortedMap()?.toList()?.sortedByDescending { it.second }?.toMap()?.forEach(){
         println("${it.key} ${it.value}")
     }
 }
