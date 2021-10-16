04cfc950803baad1ee032586596629bc014ba5e2
Dmitry Aratin <dima.aratin0@gmail.com>
2021-10-16 08:19:45 +0300

    #1 №5

--- src/main/kotlin/task5.kt
+++ src/main/kotlin/task5.kt
@@ -1,5 +1,3 @@
 fun main(args: Array<String>){
-    args.groupingBy { it }.eachCount().toSortedMap().toList().sortedByDescending{ it.second }.toMap().forEach(){
-        println("${it.key} ${it.value}")
-    }
+    args.groupingBy { it }.eachCount().toSortedMap().toList().sortedByDescending{ it.second }.toMap().map { "${it.key} ${it.value}" }.forEach(::println)
 }
