df01cdd50e852cbd4b312511a8b859305e2f74b7
Dmitry Aratin <dima.aratin0@gmail.com>
2021-10-29 10:44:01 +0300

    Добавил завершение со статус кодом 1, если один из тестов упал

--- TEST.sh
+++ TEST.sh
@@ -13,6 +13,7 @@ expected[5]="the 2${n}brown 1${n}dog 1${n}fox 1${n}jumps 1${n}lazy 1${n}over 1${
 expected[6]="the 2${n}brown 1${n}dog 1${n}fox 1${n}jumps 1${n}lazy 1${n}over 1${n}quick 1"
 
 successful=0
+failed=0
 
 for (( i=1; i <= 6; i++ ))
 do
@@ -30,8 +31,14 @@ do
 
     if [[ "${expected[${i}]}" == "$out" ]]; then
         (( successful += 1 ))
+    else
+        (( failed += 1 ))
     fi
 
 done
+
+if [[ $failed != 0 ]]; then
+    exit 1
+fi
 
 echo "Result positive test: $successful/6"
