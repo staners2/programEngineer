de26af547deed6dde6ce802727589c3e28fa15a1
Dmitry Aratin <dima.aratin0@gmail.com>
2021-10-29 09:43:07 +0300

    Добавил вывод правильно выполненных тестов

--- TEST.sh
+++ TEST.sh
@@ -3,23 +3,35 @@
 declare -a expected
 
 params="the quick brown fox jumps over the lazy dog"
+n=$'\n'
 
-expected[1]="the\nquick\nbrown\nfox\njumps\nover\nthe\nlazy\ndog"
-expected[2]="brown\ndog\nfox\njumps\nlazy\nover\nquick\nthe\nthe"
-expected[3]="brown\ndog\nfox\njumps\nlazy\nover\nquick\nthe"
-expected[4]="brown 1\ndog 1\nfox 1\njumps 1\nlazy 1\nover 1\nquick 1\nthe 2"
-expected[5]="the 2\nbrown 1\ndog 1\nfox 1\njumps 1\nlazy 1\nover 1\nquick 1"
-expected[6]="the 2\nbrown 1\ndog 1\nfox 1\njumps 1\nlazy 1\nover 1\nquick 1"
+expected[1]="the${n}quick${n}brown${n}fox${n}jumps${n}over${n}the${n}lazy${n}dog"
+expected[2]="brown${n}dog${n}fox${n}jumps${n}lazy${n}over${n}quick${n}the${n}the"
+expected[3]="brown${n}dog${n}fox${n}jumps${n}lazy${n}over${n}quick${n}the"
+expected[4]="brown 1${n}dog 1${n}fox 1${n}jumps 1${n}lazy 1${n}over 1${n}quick 1${n}the 2"
+expected[5]="the 2${n}brown 1${n}dog 1${n}fox 1${n}jumps 1${n}lazy 1${n}over 1${n}quick 1"
+expected[6]="the 2${n}brown 1${n}dog 1${n}fox 1${n}jumps 1${n}lazy 1${n}over 1${n}quick 1"
+
+successful=0
 
 for (( i=1; i <= 6; i++ ))
 do
-    printf "task$i test running..."
+    echo "task$i test running..."
     
-    out=$(java -jar ./out/task$i.jar $params)
+    out=$(java -jar ./out/task${i}.jar $params)
 
-    printf "\nExpected result: \n${expected[$i]}"
-    printf "\n\nActual result: \n$out"
+    echo "Expected result:"
+    printf "${expected[${i}]}\n\n"
 
-    printf "\n--------------------------------------------\n"
+    echo "Actual result:"
+    echo "$out"
+
+    echo "--------------------------------------------"
+
+    if [[ "${expected[${i}]}" == "$out" ]]; then
+        (( successful += 1 ))
+    fi
 
 done
+
+echo "Result positive test: $successful/6"
