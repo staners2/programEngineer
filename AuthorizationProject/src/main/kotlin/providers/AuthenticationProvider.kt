210a8c052fadd8143c64df4fb992b17447b0e249
Dmitry Aratin <dima.aratin0@gmail.com>
2021-11-14 13:13:53 +0300

    Добавлены методы для Атутентификации

--- AuthorizationProject/src/main/kotlin/providers/AuthenticationProvider.kt
+++ AuthorizationProject/src/main/kotlin/providers/AuthenticationProvider.kt
@@ -1,5 +1,31 @@
 package main.kotlin.providers
 
+import main.kotlin.models.CodeExecute
+
 object AuthenticationProvider {
 
+    fun authenticate(login: String, password: String): CodeExecute {
+        if (!loginValidate(login)){
+            return CodeExecute.NOT_FORMAT_LOGIN
+        }
+        if (!DataBaseProvider.hasLogin(login)){
+            return CodeExecute.NOT_LOGIN
+        }
+        if (!passwordValidate(login, password)){
+            return CodeExecute.NOT_PASSWORD
+        }
+
+        return CodeExecute.OK
+    }
+
+    fun loginValidate(login: String): Boolean {
+        return (Regex("^[a-zA-z0-9]{0,20}$").matches(login))
+    }
+
+    fun passwordValidate(login: String, password: String): Boolean {
+        val salt = DataBaseProvider.getSaltByLogin(login)
+        val resultPassword = DataBaseProvider.getPasswordByLogin(login)
+
+        return Utils.encode(password, salt) == resultPassword
+    }
 }
