8dfd69b66cdd82fb2a0c07ecb6e3d9b2cff5cf46
Dmitry Aratin <dima.aratin0@gmail.com>
2021-11-14 12:31:59 +0300

    Добавлен объект Аргумент

---
+++ AuthorizationProject/src/main/kotlin/models/Arguments.kt
@@ -0,0 +1,11 @@
+package main.kotlin.models
+
+data class Arguments(
+    val login: String?,
+    val password: String?,
+    val role: String?,
+    val resourse: String?,
+    val ds: String?,
+    val de: String?,
+    val vol: String?
+)
