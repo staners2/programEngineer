9b82353efe78c39f318545946fddcb4c18a432bd
Dmitry Aratin <dima.aratin0@gmail.com>
2021-11-14 12:42:31 +0300

    Добавлен объект Источник данных

---
+++ AuthorizationProject/src/main/kotlin/soursedata/SourseData.kt
@@ -0,0 +1,8 @@
+package main.kotlin.soursedata
+import main.kotlin.models.User
+import main.kotlin.models.RoleResourses
+
+object SourseData {
+    public val roleResourses: List<RoleResourses> = listOf()
+    public val users: List<User> = listOf()
+}
