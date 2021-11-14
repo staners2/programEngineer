ca859ce39bbdf110cee44a73843be645b394ca08
Dmitry Aratin <dima.aratin0@gmail.com>
2021-11-14 12:32:43 +0300

    Добавлен объект КодСтÑатус

---
+++ AuthorizationProject/src/main/kotlin/models/CodeExecute.kt
@@ -0,0 +1,12 @@
+package main.kotlin.models
+
+enum class CodeExecute(val statusCode: Int){
+    OK(0),
+    HELP(1),
+    NOT_FORMAT_LOGIN(2),
+    NOT_LOGIN(3),
+    NOT_PASSWORD(4),
+    UNKNOWN_ROLES(5),
+    FORBIDDEN(6),
+    INCORRECT_ACTIVITY(7)
+}
