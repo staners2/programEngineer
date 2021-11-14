a27e573c9fcae1c9e478b07f9d0958c6ea9fa082
Dmitry Aratin <dima.aratin0@gmail.com>
2021-11-14 13:00:58 +0300

    Добавлены методы для обÐъекта Утилиты

--- AuthorizationProject/src/main/kotlin/Utils.kt
+++ AuthorizationProject/src/main/kotlin/Utils.kt
@@ -3,5 +3,11 @@ import kotlin.random.Random
 import kotlinx.cli.*
 
 object Utils {
+    fun genearateSalt(): String = Random.nextBytes(32).toStr()
 
+    fun encode(password: String, salt: String): String = getHash(getHash(password) + salt)
+
+    fun getHash(sourse: String): String = MessageDigest.getInstance("MD5").digest(sourse.toByteArray()).toStr()
+
+    private fun ByteArray.toStr(): String = joinToString("") { "%02x".format(it) }
 }
