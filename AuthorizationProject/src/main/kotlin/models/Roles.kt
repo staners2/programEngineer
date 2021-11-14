5c7a279975ab304b05225b9b75f1aadd52e02bd0
Dmitry Aratin <dima.aratin0@gmail.com>
2021-11-14 12:56:39 +0300

    Добавлен метод для валидации введенногÐй Роли

--- AuthorizationProject/src/main/kotlin/models/Roles.kt
+++ AuthorizationProject/src/main/kotlin/models/Roles.kt
@@ -3,5 +3,11 @@ package main.kotlin.models
 enum class Roles() {
     READ,
     WRITE,
-    EXECUTE
+    EXECUTE;
+
+    companion object {
+        fun roleValidate(role: String): Boolean {
+            return Roles.values().any { it.name == role }
+        }
+    }
 }
