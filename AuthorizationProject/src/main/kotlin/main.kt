5ec96d32368383af784910e10fdff265b86c9935
Dmitry Aratin <dima.aratin0@gmail.com>
2021-11-14 14:20:17 +0300

    Описан алгоритм точки входа в приложение

--- AuthorizationProject/src/main/kotlin/main.kt
+++ AuthorizationProject/src/main/kotlin/main.kt
@@ -1,3 +1,21 @@
-fun main(args: Array<String>){
+import main.kotlin.models.Arguments
+import main.kotlin.models.CodeExecute
+import main.kotlin.providers.AuthenticationProvider
+import main.kotlin.providers.AuthorizeProvider
 
+fun main(args: Array<String>){
+    val arguments: Arguments = Arguments.parseArguments(args)
+    var exitCode: CodeExecute = CodeExecute.OK
+
+    if (arguments.login != null && arguments.password != null){
+        exitCode = AuthenticationProvider.authenticate(arguments.login, arguments.password)
+
+        if (arguments.role != null && arguments.resourse != null){
+            // Провести авторизацию
+            exitCode = AuthorizeProvider.authorize(arguments.login, arguments.password, arguments.role, arguments.resourse, arguments.ds, arguments.de, arguments.vol)
+            // TODO ...
+        }
+    }
+
+    System.exit(exitCode.statusCode)
 }
