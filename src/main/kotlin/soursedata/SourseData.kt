92a394b33fbb4beddff86dab49105d7ca963d313
Dmitry Aratin <dima.aratin0@gmail.com>
2021-11-19 17:21:40 +0300

    #3 №2

--- src/main/kotlin/soursedata/SourseData.kt
+++ src/main/kotlin/soursedata/SourseData.kt
@@ -7,26 +7,11 @@ import main.kotlin.models.Roles
 object SourseData {
     val roleResours: List<RoleResource> = listOf(
         RoleResource(Roles.READ, "A", 0),
-        RoleResource(Roles.READ, "B", 0),
-        RoleResource(Roles.READ, "C", 0),
-        RoleResource(Roles.WRITE, "A", 0),
-        RoleResource(Roles.WRITE, "B", 0),
-        RoleResource(Roles.WRITE, "C", 0),
-        RoleResource(Roles.EXECUTE, "A", 0),
-        RoleResource(Roles.EXECUTE, "B", 0),
-        RoleResource(Roles.EXECUTE, "C", 0),
-        RoleResource(Roles.READ, "A", 1),
-        RoleResource(Roles.EXECUTE, "A.B", 1),
-        RoleResource(Roles.WRITE, "XY.UV.ABCDEFGHIJ", 1),
-        RoleResource(Roles.READ, "A", 2),
-        RoleResource(Roles.WRITE, "A.B", 2),
-        RoleResource(Roles.EXECUTE, "A.B.C", 3)
+        RoleResource(Roles.WRITE, "A.B", 0)
     )
     
     val users: List<User> = listOf(
         User(0, "admin", "2b0f98d3c29b6eff9634917feef9fe6b", "salt"), // 00000
-        User(1, "user1", "319027b1c55986eb8a492273168964f9", "salt1"), // 11111
-        User(2, "user2", "97d1158adbaf32d93eb37451fddac594", "salt2"), // 22222
-        User(3, "user3", "32e703cc91abd2dd704495119641fb96", "salt3"), // 33333
+        User(1, "user1", "319027b1c55986eb8a492273168964f9", "salt1") // 11111
     )
 }
