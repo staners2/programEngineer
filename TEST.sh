f4e1f4201f3a781f4b536768f3ed11047cb91426
Dmitry Aratin <dima.aratin0@gmail.com>
2021-11-28 08:46:13 +0300

    refactor line expected code
    (cherry picked from commit d552823de7d7e0856aaf4165cc8f7a6fb8e10275)
    (cherry picked from commit 570893a9d02ce07f59e813dfbb0da9140f67432e)

--- TEST.sh
+++ TEST.sh
@@ -3,43 +3,42 @@
 declare -a input
 
 # Логин и пароль
-input[0]="-login admin -password 00000"
-input[1]="-password 00000 -login admin"
-input[2]="-login ?login? -password 00000"
-input[3]="-login NotRegUser -password NotRegUser"
-input[4]="-login admin -password WrongPassword"
+input[0]="0-login admin -password 00000"
+input[1]="0-password 00000 -login admin"
+input[2]="2-login ?login? -password 00000"
+input[3]="3-login NotRegUser -password NotRegUser"
+input[4]="4-login admin -password WrongPassword"
 
 # Логин, Пароль, Ресурс
-input[5]="-login admin -password 00000 -role READ -resource A"
-input[6]="-login admin -password 00000 -role WRITE -resource A.B"
-input[7]="-login admin -password 00000 -role UNKNOWNROLE -resource A"
-input[8]="-login admin -password 00000 -role EXECUTE -resource A"
+input[5]="0-login admin -password 00000 -role READ -resource A"
+input[6]="0-login admin -password 00000 -role WRITE -resource A.B"
+input[7]="5-login admin -password 00000 -role UNKNOWNROLE -resource A"
+input[8]="6-login admin -password 00000 -role EXECUTE -resource A"
 
 # Логин, Пароль, Ресурс, Дата начала, Дата завершения, Объем
-input[9]="-login admin -password 00000 -role READ -resource A -ds 2015-12-31 -de 2016-12-31 -vol 55"
-input[10]="-login admin -password 00000 -role READ -resource A -ds 2017-12-31 -de 2016-12-31 -vol 55"
-input[11]="-login admin -password 00000 -role READ -resource A -ds time_start -de time_end -vol 55"
-input[12]="-login admin -password 00000 -role READ -resource A -ds 2015-12-31 -de 2016-12-31 -vol NOT_VALID"
+input[9]="0-login admin -password 00000 -role READ -resource A -ds 2015-12-31 -de 2016-12-31 -vol 55"
+input[10]="7-login admin -password 00000 -role READ -resource A -ds 2017-12-31 -de 2016-12-31 -vol 55"
+input[11]="7-login admin -password 00000 -role READ -resource A -ds time_start -de time_end -vol 55"
+input[12]="7-login admin -password 00000 -role READ -resource A -ds 2015-12-31 -de 2016-12-31 -vol NOT_VALID"
 
 # Справка
-input[13]="-login admin -h"
-input[14]=""
-
-expectedCodes=(0 0 2 3 4 0 0 5 6 0 7 7 7 0 0)
+input[13]="0-login admin -h"
+input[14]="0"
 
 count=0
 
 for ((i = 0; i < 15; i++)); do
   printf "\ntest %s running...\n" "$i"
 
-  java -cp ./lib/kotlinx-cli-0.2.1.jar:main.jar app.MainKt ${input[$i]}
-
+  java -cp ./lib/kotlinx-cli-0.2.1.jar:main.jar app.MainKt ${input[$i]:1}
   code=$?
 
-  echo $code
-  echo "${expectedCodes[$i]}"
+  expectedCode="${input[$i]:0:1}"
 
-  if [[ "$code" == "${expectedCodes[$i]}" ]]; then
+  echo $code
+  echo "${expectedCode}"
+
+  if [[ "$code" == "${expectedCode}" ]]; then
     printf "test %d passed\n" "$i"
   else
     printf "test %d failed\n" "$i"
